4c51a580af64f690918b8f77984dc01933f9957f
MartinStevko <mstevko10@gmail.com>
2019-05-04 13:07:15 +0200

    Backend

---
+++ backend/data/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.serializers import serialize
+
+from .models import *
+
+
+def test(request):
+    tests = Test.objects.all()
+    return JsonResponse(serialize('json', tests), safe=False)
