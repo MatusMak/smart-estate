14b036a01a8bd298231a274be5a64943e27ae2a6
Matúš Makatura <matusmakapps@gmail.com>
2019-05-04 17:39:56 +0200

    Editorconfig fix

--- backend/data/detection/detector.py
+++ backend/data/detection/detector.py
@@ -1,61 +1,65 @@
 from .providers.parking import ParkingProvider
 
 categories = [
-	{
-		'title': 'Transport',
-		'icon': 'ni ni-bus-front-12',
-		'weight': 1,
-		'providers': [
-			{
-				'title': 'Parking',
-				'handler': ParkingProvider(),
-				'weight': 1,
-			}
-		],
-	},
+    {
+        'title': 'Transport',
+        'icon': 'ni ni-bus-front-12',
+        'weight': 1,
+        'providers': [
+            {
+                'id': 'parking',
+                'title': 'Parking',
+                'handler': ParkingProvider(),
+                'weight': 1,
+            }
+        ],
+    },
 ]
 
 class Detector():
-	def detect(this, lat, lon):
-		result = {
-			'summary': {
-				'grade': None,
-				'score': 0,
-			},
-			'categories': [],
-		}
+    def detect(this, lat, lon):
+        lat = float(lat)
+        lon = float(lon)
 
-		for category in categories:
-			providers = []
-			score = 0
+        result = {
+            'summary': {
+                'grade': None,
+                'score': 0,
+            },
+            'categories': [],
+        }
 
-			for provider in category['providers']:
-				partial = provider['handler'].provide(lat, lon)
-				score += partial['summary']['score'] * provider['weight']
+        for category in categories:
+            providers = []
+            score = 0
 
-				partial['title'] = provider['title']
-				providers.append(partial)
+            for provider in category['providers']:
+                partial = provider['handler'].provide(lat, lon)
+                score += partial['summary']['score'] * provider['weight']
 
-			result['categories'].append({
-				'title': category['title'],
-				'icon': category['icon'],
+                partial['title'] = provider['title']
+                providers.append(partial)
 
-				'summary': {
-					'score': score,
-					'grade': this.scoreToGrade(score),
-				},
+            result['categories'].append({
+                'title': category['title'],
+                'icon': category['icon'],
 
-				'providers': providers,
-			})
+                'summary': {
+                    'score': score,
+                    'grade': this.scoreToGrade(score),
+                },
 
-			result['summary']['score'] += score * category['weight']
+                'providers': providers,
+            })
 
-		result['summary']['grade'] = this.scoreToGrade(result['summary']['score'])
-		return result
+            result['summary']['score'] += score * category['weight']
 
-	def scoreToGrade(this, score):
-		if score > 70:
-			return 'good'
-		elif score > 40:
-			return 'okay'
-		return 'bad'
+        result['summary']['grade'] = this.scoreToGrade(result['summary']['score'])
+        return result
+
+    def scoreToGrade(this, score):
+        if score > 70:
+            return 'good'
+        elif score > 40:
+            return 'okay'
+        return 'bad'
